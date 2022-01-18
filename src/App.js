27bd1d3df245137c5f9a42fa85a4cde05a9e98cc
mirellgms <mirellgmsmkt@gmail.com>
2022-01-18 16:37:43 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../src/img/logo.png';
+import '../src/css/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className='welcome'>Bienvenido</a>
+        <button className='logOut'>Cerrar sesión</button>
       </header>
     </div>
   );
