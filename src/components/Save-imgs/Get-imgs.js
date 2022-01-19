3eb686996878e96896af44cb7ab5812c802fabbd
Ana Karen Guzmán <ana.kgp25@gmail.com>
2022-01-18 19:32:59 -0600

    StylesCreate

---
+++ src/components/Save-imgs/Get-imgs.js
@@ -0,0 +1,17 @@
+import '../../css/Picture.scss'
+export default function GetImg () {
+
+
+    return(
+        <main className="main">
+            <button className="btn-nav"> Regresar al menu </button>
+           <secction className="cards">
+           <div className="card">
+                <p className="text-card1">Inmueble</p>
+                <img className="prev-img" src="https://firebasestorage.googleapis.com/v0/b/totallook-1e8a8.appspot.com/o/mudarse-de-casa%201.png?alt=media&token=c8e8578a-15f2-4f11-8c53-243760a32fcd" alt="icon"/>
+                <p className="text-card2">Nº Contrato</p>
+           </div>
+           </secction>
+        </main>
+    )
+}
