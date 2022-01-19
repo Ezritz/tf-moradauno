d5b5994bf074adb92d82119ede8afc3fdf8e93f2
GeraldineEJ <geraldineestrada29@gmail.com>
2022-01-18 20:08:04 -0600

    instrucciones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Instructive from './components/Instructive';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Instructive />
   </React.StrictMode>,
   document.getElementById('root')
 );
