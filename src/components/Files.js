c747382108a784205de6991c2af3ac66a265b9f5
ezritz <lidia.edith.escoto@gmail.com>
2022-01-21 11:31:27 -0600

    Change formulary to upload images and sass

--- src/components/Files.js
+++ src/components/Files.js
@@ -1,22 +1,21 @@
 import Banner from './Banner'
-import image01 from "../img/image01.png"
 import '../css/Files.scss';
 
-export default function Files() {
-  return (
-    <>
-    <div>
-      <Banner/>
-    </div>
-    <main className= "upload-files">
-        <Banner/>
-      <div className= "img-file">
-        <img className="img-upload" src={image01} alt="img-upload"/>
-      
-        <button className="btn-upload-image">Carga tu imagen aquí</button>
 
-      </div>
+export default function Files() {
+
+  
+
+  return (
+    
+    
+    <main className= "upload-files">
+      <Banner/>
+      <form className= "img-file">
+        
+
+      </form>
     </main>
-    </>
+    
   );
 }
--- src/components/Formulary.js
+++ src/components/Formulary.js
@@ -1,29 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {addData} from '../firebase/Firestore'
 //import Select  from 'react-select'
 import Banner from "./Banner";
 import Instructive from "./Instructive";
 import "../css/Formulary.scss";
+import '../css/Files.scss';
+import image01 from "../img/image01.png"
+import {AddImg} from '../firebase/Firestore'
+
 
 export default function Formulary() {
-    const [date, setDate]= useState('');
-    const [folio, setFolio]=useState();
-    // const [contract, setContract] =useState('');
-    const [nameAsesor, setNameAsesor] = useState('');
-    const [error, setError]=useState('');
-    const nav= useNavigate();
+  const [date, setDate]= useState('');
+  const [folio, setFolio]=useState();
+  const [loading, setLoading] = useState(false);
+  // const [contract, setContract] =useState('');
+  const [nameAsesor, setNameAsesor] = useState('');
+  const [error, setError]=useState('');
+  const nav= useNavigate();
 
-    const handleSendSubmit =(e) => {
-      e.preventDefault()
-      addData(folio,nameAsesor,date).then(()=> nav('/down-imgs'))
-      //  if(folio!='' && date!= '' && nameAsesor!=''){
-        // nav('/down-imgs');
-        // 
-      //  } else {
-        // setError('Campo vacio')
-      //  }
+  const handleSendSubmit =(e) => {
+    e.preventDefault()
+    addData(folio,nameAsesor,date).then(()=> nav('/down-imgs'))
+    //  if(folio!='' && date!= '' && nameAsesor!=''){
+      // nav('/down-imgs');
+      // 
+    //  } else {
+      // setError('Campo vacio')
+    //  }
+  }
+
+  
+
+  const handleAddImg= (e) => {
+    // const targ = e.target.files[0];
+    const files = e.target.files;
+    console.log(e.target.files)
+    console.log('target', e.target);
+    for(const i in files){
+        console.log('targ: ', files[i])
+        AddImg(files[i],files[i].name).then(() =>{ console.log('succesfull')})
+
+       /* AddImg.snapshot.ref.getDownloadURL().then((url_img)=>{
+            console.log('url', url_img)
+        })
+        */
+    
+        // console.log(targ);
     }
+    setLoading(true)
+    
+  }
 
 
   return (
@@ -35,67 +62,31 @@ export default function Formulary() {
           <Instructive />
         </div>
         <div className="containerForm">
-          <form onSubmit={handleSendSubmit}>
+          <form onSubmit={handleSendSubmit}
+          className="form">
            
                 <div className="formulary">
                 
-                <label 
-                className="label-4">No. de Folio</label>
-                <input
-                min="0"
-                type="number"
-                placeholder="No.Folio"
-                onChange={(e)=> setFolio(e.target.value)}
-                value={folio}
-                ></input>
-                <label className="label-1">Fecha de captura</label>
-                <input 
-                onChange={(e)=> setDate(e.target.value)}
-                className="date-1" 
-                type="date"
-                value={date}
-                ></input>
-                
-                <label className="label-3">Nombre del Asesor</label>
-                <input
-                    onChange={(e)=> setNameAsesor(e.target.value)}
-                    className="date-3"
-                    type="text"
-                    placeholder="Nombre completo"
-                    value={nameAsesor}
-                ></input>
-                    <div className="type-cont">
-                    <label
-                    className="label-type"
-                    htmlFor="first-cont">Tipo de contrato</label>
-                    <input
-                    className="date-4"
-                    id="first-cont"
-                    name="1"
-                    type="radio"
-                    className="cont-start"
-                    value="1"
-                    /> 
-                    <label
-                    className="label-type" 
-                    htmlFor="end-cont">Inicio</label>
-                    <input
-                    className="date-5"
-                    id="end-cont" 
-                    name="1"
-                    className="cont-end" 
-                    type="radio"
-                    value="2"
-                    />
-                    <p>{error}</p>
-                    <label
-                    className="label-type" 
-                    >Termino</label>
-                    </div>
-                    
-                <button 
-                className="btn-continue"
-                >Continuar</button>
+                  <label 
+                  className="label-4">No. de Folio</label>
+                  <input
+                  className="inputFolio"
+                  min="0"
+                  type="number"
+                  placeholder="No.Folio"
+                  onChange={(e)=> setFolio(e.target.value)}
+                  value={folio}
+                  ></input>
+                  <img className="img-upload" src={image01} alt="img-upload"/>
+      
+                  {!loading && <input onChange={handleAddImg} 
+                  type="file"accept="image/png, image/jpeg, image/jpg" multiple/>}
+                  {!loading && <button className="btn-upload-image"
+                  > Seleccionar imagenes
+                  </button>}
+                  {loading && <button className="btn-upload-image-2" 
+                  >Cargar imagenes
+                  </button>}
                 </div>
           </form>
         </div>
