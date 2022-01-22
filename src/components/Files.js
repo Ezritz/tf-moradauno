954e77f943bb8a03d5a88fa9a7fec3f4704102f4
ezritz <lidia.edith.escoto@gmail.com>
2022-01-22 14:42:21 -0600

    Upload imgs and repeat docs

--- src/components/Files.js
+++ src/components/Files.js
@@ -11,10 +11,10 @@ export default function Files({urls}) {
     
     <main className= "upload-files">
       <Banner/>
-      {urls.map((url,i)=>(
+      {/*urls.map((url,i)=>(
         <img key={i}
         src={url}></img>
-      ))}
+      ))*/}
       <form className= "img-file">
         
 
--- src/firebase/Firestore.js
+++ src/firebase/Firestore.js
@@ -1,11 +1,10 @@
 import 'firebase/firestore'
 import { db, storage } from "./Config";
 
-export const addData = (setFolio, setAsesor, setFecha) => db.collection('files-morada').doc().set({
-    folio: setFolio,
-    asesor: setAsesor,
-    fecha: setFecha,
-    date: new Date(),
+export const addData = (setFolio, setUrls) => db.collection('files-morada').doc().set({
+   folio:setFolio,
+   imgs:setUrls, 
+   date: new Date(),
 });
 
 export const AddImg = (ref, img) => storage.ref(`/morada/${ref.name}`).put(img).then((snapshot)=>{
