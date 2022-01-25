c45bc6ac0459870aff5654488d3935859937e0ee
ezritz <lidia.edith.escoto@gmail.com>
2022-01-24 23:20:47 -0600

    add nav

--- src/components/Files.js
+++ src/components/Files.js
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react';
 import { dataRef, removed } from '../firebase/Firestore';
+import {useNavigate} from 'react-router-dom';
 import { Banner } from './Banner';
 import '../css/Picture.scss';
 import Modal from './Modal';
 import Swal from 'sweetalert2';
-
+import image from '../img/check.png';
 
 export default function Files() {
   const [collection, setCollection] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectModal, setSelectModal] = useState(null);
-
   const [folio, setFolio]= useState('');
-  
+  const nav= useNavigate();
 
   useEffect(() => {
     const getCollection =  () => {  
@@ -20,11 +20,12 @@ export default function Files() {
         const docs=[];
         snapshot.forEach((doc) => {
           docs.push({ ...doc.data(), id: doc.id })
-             //console.log('id:',id)       
+            //console.log('id:',id)       
         });
-        // console.log(docs)
+         console.log(docs)
 
         setCollection(docs)
+        
       })
     };
     getCollection();
@@ -56,23 +57,33 @@ export default function Files() {
     console.log('aqui folio',folio)
   }
 
+  const handleBack = () => {
+    nav('/');
+  }
+
+
   return (
     <main className="main">
       <Banner/>
       <section className='principal'>
-        <button className="btn-nav"> Regresar al menu </button>
-        <input
-        type="text"
-        onChange={(e)=> setFolio(e.target.value)}
-        placeholder="Selecciona tu folio"
-        value={folio}
-        ></input>
-        <button onClick={handleSearch}>Buscar</button>
+        <div className="div-nav">
+          <button className="btn-nav" onClick={handleBack}> Volver </button>
+          <input
+          type="text"
+          onChange={(e)=> setFolio(e.target.value)}
+          placeholder="Selecciona tu folio"
+          value={folio}
+          ></input>
+          <button onClick={handleSearch}>Buscar</button>
+          <label className="type-service">Inquilino</label>
+          <img className="inquilino" src={image}></img>
+          
+        </div>
         <div className="cards">
           {collection.map((data,i) => 
             folio === data.folio ? 
           (
-            
+        
             <div className="card" key={i}>
               
               <p className="text-card1">{data.id}</p>
